860877f12dc428c1a477b7a896ebb120cf8f92ed
HimanshuKumarShahi <himanshushahi2006@gmail.com>
2025-10-18 00:47:47 +0530

    Initial commit of Vite React project

---
+++ 01_basicReact/src/chai.jsx
@@ -0,0 +1,9 @@
+function Chai(){
+    return (
+        <>
+        <h1>Hello Wel-Come</h1>
+        <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem blanditiis distinctio vitae quisquam rem placeat doloremque reiciendis incidunt natus delectus, alias suscipit beatae.</h3>
+        </>
+    )
+}
+export default Chai
