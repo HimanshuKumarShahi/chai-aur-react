160f9710fdac41a2db4ecfe41e98e0b0949ce5d1
HimanshuKumarShahi <himanshushahi2006@gmail.com>
2025-11-30 17:24:49 +0530

    add routes to interact.

--- notesproject/backend/Server.js
+++ notesproject/backend/Server.js
@@ -5,6 +5,14 @@ const port=process.env.PORT || 3000
 
 app.get("/api",(req,res)=>{
     res.status(200).send("Hello you are in right path but ");
+});
+
+app.post("/api/notes",(req,res)=>{
+    res.status(201).json({Message:"Post Created Successfully"})
+})
+
+app.put("/api/notes",(req,res)=>{
+    res.status(200).json({Message:"Post Updated Successfully"})
 })
 
 app.listen(port,()=>{
