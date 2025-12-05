7fbcd3dc2099c4f067817b7cada6b63636ba31b2
HimanshuKumarShahi <himanshushahi2006@gmail.com>
2025-12-05 19:47:37 +0530

    solving bugs app crashed.

--- notesproject/backend/src/Server.js
+++ notesproject/backend/src/Server.js
@@ -1,9 +1,12 @@
 import express from "express";
+import notesRoutes from "./routes/notesRoutes.js";
+import notesController from "./controllers/notesController.js"
+
 const app=express()
 
 const port=process.env.PORT || 3000
 
-app.use("/api/notes",notesRoutes)
+app.use("/api/notes",notesRoutes);
 
 
 app.listen(port,()=>{
