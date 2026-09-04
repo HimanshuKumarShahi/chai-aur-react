2dbcb4bebde95d520de331259b6de70cc75ac135
Himanshu Kumar Shahi <himanshushahi2006@gmail.com>
2026-09-04 19:12:21 +0530

    in components add a navbar & display in home

---
+++ reactdetails/src/components/navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function navbar() {
+  return (
+    <>
+        <div className="bg-gray-800 text-white p-4">
+          <h1 className="text-xl font-bold">My App</h1>
+        </div>
+        <div className="bg-gray-100 p-4">
+          <p>Welcome to my app!</p>
+        </div>  
+    </>
+  )
+}
