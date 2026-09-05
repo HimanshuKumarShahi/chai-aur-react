2f2279f7fe009faf803e5a4e9ac9c454ed85f3b9
Himanshu Kumar Shahi <himanshushahi2006@gmail.com>
2026-09-05 16:19:05 +0530

    configure all the pages in manner

--- reactdetails/src/App.jsx
+++ reactdetails/src/App.jsx
@@ -1,10 +1,11 @@
-import Navbar from "./components/navbar"
+import Navbar from "./components/navbar.jsx"
+import Products from "./functionality/product.jsx"
 
 export default function App() {
   return (
     <>
     <Navbar />
-    <div>App</div>
+    <Products />
     </>
   )
 }
