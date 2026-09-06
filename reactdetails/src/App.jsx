18fb026ee9d736df1c5a4534ae6309a6373d8795
Himanshu Kumar Shahi <himanshushahi2006@gmail.com>
2026-09-06 15:11:27 +0530

    config all the routes in right place

--- reactdetails/src/App.jsx
+++ reactdetails/src/App.jsx
@@ -1,11 +1,20 @@
-import Navbar from "./components/navbar.jsx"
-import Products from "./functionality/product.jsx"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/navbar.jsx";
+import Products from "./functionality/product.jsx";
+import RandomUsersfromAPI from "./functionality/Randomusers.jsx";
+import About from "./functionality/about.jsx";
 
 export default function App() {
   return (
-    <>
-    <Navbar />
-    <Products />
-    </>
-  )
+    <BrowserRouter>
+      
+      <Navbar />
+      
+      <Routes>
+        <Route path="/" element={<RandomUsersfromAPI />} /> 
+        <Route path="/products" element={<Products />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
